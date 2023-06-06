c11f00a0197db809ef98e224487399ad92fc8763
Nawel0310 <131374182+Nawel0310@users.noreply.github.com>
2023-06-06 17:27:49 -0300

    Cambiado nombre

--- CalculadoraAñosPerro/src/Main.java
+++ CalculadoraAñosPerro/src/Main.java
@@ -7,10 +7,6 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner entrada;
-
-    public Main() {
-    }
-
     public static void main(String[] args) {
         ObtenerAniosPerro();
     }
