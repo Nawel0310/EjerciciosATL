eaa0893879618a84352ab79ff1a6c3a0a6a37589
Nawel0310 <131374182+Nawel0310@users.noreply.github.com>
2023-06-06 04:31:11 -0300

    agregado ejemplo de programa.java

---
+++ ejemploProgramaJava/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Nahuel";
+        int edad= 21;
+        String carrera= "Ing en sistemas de información";
+        System.out.println("Hola!, mi nombre es "+nombre+" tengo "+edad+" años y actualmente estudio "+carrera);
+    }
+}
