487e2f39ee6b50f4722c3aa5ff093a50da55e84e
Nawel0310 <131374182+Nawel0310@users.noreply.github.com>
2023-06-20 16:29:00 -0300

    Creado módulo de Springboot para practicar

--- CalculadoraAñosPerro/src/Main.java
+++ CalculadoraAñosPerro/src/Main.java
@@ -1,26 +1,10 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
 
 import java.util.Scanner;
 
 public class Main {
-    static Scanner entrada;
+    static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
-        ObtenerAniosPerro();
+
     }
 
-    public static void ObtenerAniosPerro() {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese la edad en años humanos.");
-        Integer edadHumano = entrada.nextInt();
-        Calculadora calculadora = new Calculadora(edadHumano);
-        Integer edadPerro = calculadora.CalcularAniosPerro();
-        System.out.println("Edad en años perro: " + edadPerro);
-    }
-
-    static {
-        entrada = new Scanner(System.in);
-    }
 }
